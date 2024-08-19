93300b590f1b82cb65267afb094c2acdc43da8cd
ribeirodonascimento <121270162+ribeirodonascimento@users.noreply.github.com>
2024-08-18 21:06:12 -0300

    Update somaDeDoisNumeros.java

--- somaDeDoisNumeros.java
+++ somaDeDoisNumeros.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class somaDeDoisNumeros{
     public static void main(String[]args){
         Scanner s = new Scanner(System.in);
+        System.out.println("Digite um número.");
         int n1 = s.nextInt();
+        System.out.println("Agora digite outro número.");
         int n2 = s.nextInt();
         int soma = n1 + n2;
-        System.out.println("X = "+soma);
+        System.out.println("A soma dos dois números é: "+soma);
     }
 }
